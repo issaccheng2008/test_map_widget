e4c68bb7fa9fbbe8d0b825bec20d4f9c5762c155
IC2008 <165460461+issaccheng2008@users.noreply.github.com>
2025-10-12 16:09:34 +0800

    Limit overlay mouse capture to image area

---
+++ OverlayImageWidget.h
@@ -0,0 +1,52 @@
+#ifndef OVERLAYIMAGEWIDGET_H
+#define OVERLAYIMAGEWIDGET_H
+
+#include <QGraphicsView>
+#include <QString>
+
+class QGraphicsPixmapItem;
+class QGraphicsItem;
+
+class RotationHandle;
+
+class OverlayImageWidget : public QGraphicsView
+{
+    Q_OBJECT
+public:
+    explicit OverlayImageWidget(QWidget *parent = nullptr);
+
+    [[nodiscard]] bool loadImage(const QString &filePath);
+    void clearImage();
+    [[nodiscard]] bool hasImage() const;
+
+protected:
+    void resizeEvent(QResizeEvent *event) override;
+    void wheelEvent(QWheelEvent *event) override;
+    void mousePressEvent(QMouseEvent *event) override;
+    void mouseMoveEvent(QMouseEvent *event) override;
+    void mouseReleaseEvent(QMouseEvent *event) override;
+
+private:
+    friend class RotationHandle;
+
+    void updateTransform();
+    void updateMouseTransparency();
+    void updateInteractionRegion();
+    void beginRotation(const QPointF &scenePos);
+    void updateRotationFromScenePos(const QPointF &scenePos);
+    void endRotation();
+
+    QGraphicsScene *m_scene = nullptr;
+    QGraphicsPixmapItem *m_pixmapItem = nullptr;
+    QGraphicsItem *m_rotationHandle = nullptr;
+    QPointF m_lastMousePosition;
+    bool m_isDragging = false;
+    bool m_isRotating = false;
+    qreal m_currentScale = 1.0;
+    qreal m_currentRotation = 0.0;
+    qreal m_rotationInitial = 0.0;
+    qreal m_rotationStartAngle = 0.0;
+    QPointF m_rotationCenter;
+};
+
+#endif // OVERLAYIMAGEWIDGET_H
