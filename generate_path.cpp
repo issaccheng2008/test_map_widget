cc53260fc1fc1e795dc2e867d69b9387661e8f52
issac <issaccheng2008@gmail.com>
2025-10-31 16:52:05 +0800

    remove extra perameters

--- generate_path.cpp
+++ generate_path.cpp
@@ -49,11 +49,9 @@ const double max_image_area = 10000.0;
 const int channel_number = 5;
 
 void generate_path(const QList<Esri::ArcGISRuntime::Point> &workAreaPolygon,
-                   const QList<QList<Esri::ArcGISRuntime::Point>> &obstacles,
                    const QVector<QVector<int>> &channelGrid)
 {
     Q_UNUSED(workAreaPolygon)
-    Q_UNUSED(obstacles)
     Q_UNUSED(channelGrid)
 
     qDebug() << "generate_path called";
