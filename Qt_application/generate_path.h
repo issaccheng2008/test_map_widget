663c6644fbad17d9a8fc36d2859e6d1969812b74
IC2008 <165460461+issaccheng2008@users.noreply.github.com>
2025-11-02 11:57:40 +0800

    Add ESP32 camera monitor and centralize device URL

--- Qt_application/generate_path.h
+++ Qt_application/generate_path.h
@@ -3,6 +3,7 @@
 
 #include <QList>
 #include <QVector>
+#include <QString>
 
 #include "Point.h"
 
@@ -11,10 +12,14 @@ extern const double car_length;
 extern const double grid_size;
 extern const double max_image_area;
 extern const int channel_number;
+extern const QString kEsp32BaseUrl;
+
+class QStatusBar;
 
 QVector<Esri::ArcGISRuntime::Point> generate_path(const QList<Esri::ArcGISRuntime::Point> &pinnedImageCorners,
                                                   const QVector<QVector<int>> &channelGrid,
-                                                  const Esri::ArcGISRuntime::Point &currentGpsPoint);
+                                                  const Esri::ArcGISRuntime::Point &currentGpsPoint,
+                                                  QStatusBar *statusBar = nullptr);
 
 Esri::ArcGISRuntime::Point gridCellGpsCoordinate(int row,
                                                  int column,
